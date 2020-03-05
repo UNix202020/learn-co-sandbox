9dcc15302981a5f11996522b6f100b1e1acd4670
T S <taneymail@gmail.com>
2020-03-05 19:13:19 +0000

    modified environment.rb

---
+++ cat_breeds/config/environment.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/cat_breeds/scraper'
+require_relative '../lib/cat_breeds/restaurant'
+require_relative '../lib/cat_breeds/cli'
+require_relative '../lib/cat_breeds/version'
