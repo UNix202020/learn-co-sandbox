bc045e26759fbbff5c896f98bfcafa5b3e9dadcb
T S <taneymail@gmail.com>
2020-03-06 00:28:54 +0000

    save requirements

--- cat_breeds/config/environment.rb
+++ cat_breeds/config/environment.rb
@@ -3,7 +3,3 @@ require 'nokogiri'
 require 'open-uri'
 require 'HTTParty'
 
-require_relative '../lib/cat_breeds/scraper'
-require_relative '../lib/cat_breeds/restaurant'
-require_relative '../lib/cat_breeds/cli'
-require_relative '../lib/cat_breeds/version'
