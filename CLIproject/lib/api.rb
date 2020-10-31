8175c58bbe9ed850d04c905c2f08f3adceec8e8c
T S <taneymail@gmail.com>
2020-10-31 05:00:58 +0000

    updating infor in api.rb

--- CLIproject/lib/api.rb
+++ CLIproject/lib/api.rb
@@ -2,7 +2,19 @@ class API
   
   def self.fetch_characters
       url = 'https://ghibliapi.herokuapp.com/people'
-      binding.pry
+      uri = URI(url)
+      response = Net::HTTP.get(uri)
+      hash = JSON.parse(response)
+      
+      array_of_characters = hash["characters"]
+      
+      array_of_characters.each do |character_hash|
+        character = Character.new 
+        character.name = character_hash["strCharacter"]
+        character.gender = character_hash["strGender"]
+        character.age = character_hash["strAge"]
+        character.films = character_hash["strFilms"]
+      end
   end
   
 end 
