a2025c4c6a0a8f380f6fa703af6a045a9ef40b24
T S <taneymail@gmail.com>
2020-10-31 03:46:23 +0000

    updated cli.rb

--- CLIproject/lib/cli.rb
+++ CLIproject/lib/cli.rb
@@ -25,14 +25,30 @@ class CLI
     Character.all.each.with_index(1) do |character, index|
       puts "#{index}. #{character.name}"
     end
+  end
     
     def ask_user_to_choose_character
       puts "Enter the number of the character you'd like to read about."
       index = gets.strip.to_i - 1
       
+      until index.between?(0, Character.all.length - 1)
+        puts "Oops! I don't understand. Please enter a valid number."
+        index = gets.strip.to_i - 1
+      end
+      
       character_instance = Character.all[index]
+      
+      display_character_traits(character_instance)
     end
     
-  end
-  
+    def display_character_traits(character)
+        sleep(2)
+        puts character.name
+        puts "Name" + |character.name|
+        puts "Gender" + |character.gender|
+        puts "Age" + |character.age|
+        puts "Films" + |character.films|
+    end
+        
+    end
 end
