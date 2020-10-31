6d26d7e73515c564dff26b8d4d3f06b5b3e3170f
T S <taneymail@gmail.com>
2020-10-31 03:17:27 +0000

    more build out of cli.rb

--- CLIproject/lib/cli.rb
+++ CLIproject/lib/cli.rb
@@ -9,20 +9,28 @@ class CLI
   def list
       puts "I've complied a list of characters from Studio Ghibli films."
       puts "Would you like to see the list?"
+      puts "Type 'yes' to continue or 'no' to leave."
     
       if user_responds = "yes" || user_responds == "y"
           puts "Okay! I'll show you the list!" 
         
           display_the_characters
+          ask_user_to_choose_character
       end
     
   end
   
   def display_the_characters
     
-    Character.all.each.do |character|
-      puts character.name
+    Character.all.each.with_index(1) do |character, index|
+      puts "#{index}. #{character.name}"
     end
+    
+    def ask_user_to_choose_character
+      puts "Enter the number of the character you'd like to read about."
+      index = gets.strip.to_i - 1
+    end
+    
   end
   
 end
