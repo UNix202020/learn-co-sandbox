140f693e9ea9c053310e293a7aaeb073fa55db29
T S <taneymail@gmail.com>
2020-10-31 02:34:12 +0000

    building out cli.rb

--- CLIproject/lib/cli.rb
+++ CLIproject/lib/cli.rb
@@ -2,6 +2,23 @@ class CLI
   
   def begin 
       puts "Greetings!"
-  end      
+      API.fetch_characters
+      self.list
+  end  
+  
+  def list
+      puts "I've complied a list of characters from Studio Ghibli films."
+      puts "Would you like to see the list?"
+    
+      if user_responds = "yes" || user_responds == "y"
+          puts "Okay! I'll show you the list!" 
+        
+          display_the_characters
+      end
+    
+  end
+  
+  def display_the_characters
+    binding.pry
   
 end
