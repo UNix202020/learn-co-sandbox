02861bb2a3ef47eceb7acbcd53b6db69ec8d78e2
T S <taneymail@gmail.com>
2020-10-31 02:39:35 +0000

    building out character.rb

--- CLIproject/lib/character.rb
+++ CLIproject/lib/character.rb
@@ -1,3 +1,14 @@
 class CHARACTER 
+  attr_accessor :Id, :name, :gender, :age, :eye_color, :hair_color, :films, :species, :url
+  
+  @@all =[]
+  
+  def initialize
+    @@all << self
+  end
+  
+  def self.all
+      @@all
+  end
   
 end
