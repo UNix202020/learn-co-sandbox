05cb6c7800a30789aef1501e6adbc0bdfbca8096
T S <taneymail@gmail.com>
2020-11-12 10:38:12 +0000

    final changes

--- CLIproject/lib/cli.rb
+++ CLIproject/lib/cli.rb
@@ -1,71 +1,77 @@
-class CLI 
+class CLI
   
   def start
-      puts "I've compiled a list of characters from Studio Ghibli films."
-      puts "Would you like to see the list?"
-      puts "Type 'y' to continue or 'n' to leave."
-      
-      user_input = gets.strip.downcase
-      if user_input == "y"
-          puts "Okay! I'll show you the list!"
-          sleep(1)
-          "\n"
-          puts "Fetching..."
-          sleep(4)
-          
-      display_characters
-      ask_user_choice
-      
-      else
-         puts "ByeBye! See you later!"
-         puts "\n"
-      end
-    end
+    puts "\n"
+    puts "Welcome!"
+    puts "\n"
+    API.fetch_characters
+    self.list
+  end
   
-  def display_characters
+  def list
+    # give user option to see list of Cover Girl characters
+    puts "Would you like to see the list of Studio Ghibli characters?"
+    puts "Type ' y' for yes to continue. Or press any another key to exit."
     
-    CHARACTER.all.each.with_index(1) do |character, index|
-      puts "#{index}.#{character.name}"
+    user_input = gets.strip.downcase
+    
+    # if user says yes
+    if user_input == "y"
+      puts "\n"
+      puts "Excellent!"
+      puts "\n"
+      # display the list of character names
+      list_of_SG_characters
+      ask_user_for_character_choice
+      
+      sleep(2)
+      puts "\n"
+      
+      list
+    else
+      puts "Bye! I hope you found your character!"
+      puts "\n"
+    end
+    
+  end
+  
+  def list_of_SG_characters
+    # binding.pry
+    CHARACTER.all.each_with_index do |name, index|
+      puts "#{index}. #{name}"
     end
   end
+  
+  def ask_user_for_character_choice
+    # ask user for choice
+    puts "\n"
+    puts "Enter the number of the character you'd like to know more about."
+    index = gets.strip.to_i - 1
     
-    def ask_user_choice
-      puts "Enter the number of the character you'd like to read about."
-      index = gets.strip.to_i-1
-      
-      until index.between?(0, CHARACTER.all.length-1)
-        puts "Oops! I don't understand. Please enter a valid number."
-        index = gets.strip.to_i-1
-      end
-      
-      character_instance = CHARACTER.all[index]
-      
-      display_character_traits(character_instance)
+    # index valid? number between 0 and 20
+    until index.between?(0, CHARACTER.all.length - 1)
+      # keep asking for user input
+      puts "Sorry, invalid input. Choose a valid number."
+      index = gets.strip.to_i - 1
     end
     
-    def display_character_traits(character)
-        sleep(2)
-        puts "\n"
-        puts "Name"+character.name
-        puts "Gender"+character.gender
-        puts "Age"+character.age
-        puts "Films"+character.films
-    end
+    character_instance = CHARACTER.all[index]
     
-    def list
-      puts "Would you like to look again?"
-      puts "Type 'y' to continue or 'n' to leave."
-      
-      user_input = gets.strip.downcase
+    display_character_details(character_instance)
     
-      if user_input == "y"
-          puts "Okay! I'll show you the list!"
-          display_characters
-          ask_user_choice
-      else
-         puts "ByeBye! See you later!"
-         puts "\n"
-      end
-    end
-        
+    
+  end
+  
+  def display_character_details(character_instance)
+    sleep(1)
+    puts "\n"
+    puts "Name:"+character_instance.name
+    puts "\n"
+    puts "Gender:"+character_instance.gender
+    puts "\n"
+    puts "Age:"+character_instance.age
+    puts "\n"
+    puts "Films:"+character_instance.films
+  end
+  
 end
